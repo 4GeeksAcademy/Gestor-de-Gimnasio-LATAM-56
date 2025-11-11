e9b97704b40d37371e427aa5407afae571b8658e
fernandoqm01 <fernandoquesadamora@gmail.com>
2025-11-11 03:34:33 +0000

    nav actualizado

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -5,13 +5,38 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-dark bg-none">
 			<div className="container">
-				<div>
-					<img src="/logo3.png" alt="Logo del gimnasio" height="50" />
+				<div className="d-flex align-items-center">
+					<Link to="/">
+						<img
+							src="/logo3.png"
+							alt="Logo del gimnasio"
+							height="50"
+							style={{ cursor: "pointer" }}
+						/>
+					</Link>
+					<div className="dropdown">
+						<button
+							className="btn btn-secondary dropdown-toggle"
+							type="button"
+							id="dropdownMenuButton"
+							data-bs-toggle="dropdown"
+							aria-expanded="false"
+							style={{ backgroundColor: "transparent", border: "none" }}
+						>
+							{/* Ícono tipo hamburguesa */}
+							<span className="navbar-toggler-icon"></span>
+						</button>
+
+						<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+							<li><button className="dropdown-item" type="button">Inicio</button></li>
+							<li><button className="dropdown-item" type="button">Rutinas</button></li>
+							<li><button className="dropdown-item" type="button">Alimentación</button></li>
+							<li><button className="dropdown-item" type="button">Contacto</button></li>
+						</ul>
+					</div>
+
 				</div>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary me-2">Check the Context in action</button>
-					</Link>
 					{/* AGREGADO: Botón de registro */}
 					<Link to="/login">
 						<button className="btn btn-info me-2">Iniciar Sesión</button>
